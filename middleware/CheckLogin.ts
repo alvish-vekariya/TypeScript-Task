959dd0ffedad0f6bad0c1f9a1c2cf043b0358b74
Alvish Vekariya <sit404@sit.local>
2024-05-01 18:50:17 +0530

    make common connection

--- middleware/CheckLogin.ts
+++ middleware/CheckLogin.ts
@@ -4,11 +4,12 @@ import mongoose from "mongoose";
 
 const checklogin = async(req:express.Request, res: express.Response,next: express.NextFunction)=>{
     const userID: string = req.body.userID;
+    
     const checkuserid: boolean = mongoose.isValidObjectId(userID);
     if(!checkuserid){
         return res.send("enter valid userid");
     }
-    const checkLogin = await UserModel.findOne({"_id":userID, "token":{$exists : true}});
+    const checkLogin = await UserModel.findOne({_id:userID});
     // console.log(checkLogin);
     if(checkLogin == null){
         return res.send('login is required!!');            
